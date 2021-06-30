20b58dc19c1dff29b5158cfbab413108437aa4c8
Tvibe <tvibe58@163.com>
2021-06-30 17:24:56 +0800

    react 路由

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/">Home</Link>&nbsp;
+      <Link to="/about">About</Link>
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+    </main>
+  </div>
+)
+export default App
---
+++ src/pages/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+    <p>这是About页面</p>
+  </div>
+)
+
+export default About
