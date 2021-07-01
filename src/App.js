b02b72158e133c84e711738e362434def3a465f8
Tvibe <tvibe58@163.com>
2021-07-01 11:46:37 +0800

    ant design

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@
 
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
+import { DatePicker, Button } from 'antd'
+import './App.css';
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 
@@ -34,9 +36,11 @@ const App = () => (
     <header>
       <Link to="/">Home</Link>&nbsp;
       <Link to="/about">About</Link>
+      <Button type="primary">Button</Button>
     </header>
 
     <main>
+      <DatePicker />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
     </main>
