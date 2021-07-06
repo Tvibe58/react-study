2be337ecbb89481315d9d59ad47c610ecad5c2ab
Tvibe <tvibe58@163.com>
2021-07-06 11:46:34 +0800

    createHashHistory

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from "react-router";
-import { createBrowserHistory } from "history";
-const history = createBrowserHistory({
+import { createBrowserHistory, createHashHistory } from "history";
+const history = createHashHistory({
   basename: '/react-study'
 })
 
